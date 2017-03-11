bc79c2a0b7ee00dcbcf67a24776c80474eb2afea
Max <tutv95@gmail.com>
2017-03-11 22:58:53 +0700

    deploy

--- event-web/public/styles.b2328beb0372c051d06d.bundle.js
+++ event-web/public/styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([5,6],{1030:function(n,o,c){n.exports=c(436)},436:function(n,o){}},[1030]);
+webpackJsonp([5,6],{1031:function(n,o,c){n.exports=c(435)},435:function(n,o){}},[1031]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
