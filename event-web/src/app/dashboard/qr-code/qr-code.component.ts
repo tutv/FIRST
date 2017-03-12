27c3224dc5aa9a3b953cd225192f220e06ae2f01
Max <tutv95@gmail.com>
2017-03-12 10:01:18 +0700

    Updated qr code

--- event-web/src/app/dashboard/qr-code/qr-code.component.ts
+++ event-web/src/app/dashboard/qr-code/qr-code.component.ts
@@ -18,6 +18,6 @@ export class QrCodeComponent implements OnInit, OnChanges {
     }
 
     ngOnChanges() {
-        this.url = `https://api.qrserver.com/v1/create-qr-code/?size=${this.size}&data=${this.data}`;
+        this.url = `https://chart.googleapis.com/chart?cht=qr&choe=UTF-8&chs=${this.size}&chld=H&chl=${this.data}`;
     }
 }
